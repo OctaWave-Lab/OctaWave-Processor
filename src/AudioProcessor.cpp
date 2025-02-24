209dda5ab80d3d0cb36d623818acdf021ddbe8ad
khyzhun <sasha.khyzhun@gmail.com>
2025-02-24 21:55:45 +0200

    Project initiation and setting up build things

---
+++ src/AudioProcessor.cpp
@@ -0,0 +1,7 @@
+#include "../src/AudioProcessor.h"
+#include <iostream>
+using namespace std;
+
+void AudioProcessor::process() {
+    cout << "Processing the audio" << endl;
+}
---
+++ src/AudioProcessor.h
@@ -0,0 +1,9 @@
+#ifndef AUDIO_PROCESSOR_H
+#define AUDIO_PROCESSOR_H
+
+class AudioProcessor {
+public:
+    void process();
+};
+
+#endif
